88fd481254809b638d8e6105d8927b56d2e47b7e
Tanner Bebb <17tbebb@gmail.com>
2023-12-22 15:36:41 -0800

    Added lines 36 - 39 and lines 41 - 45. Explaintion is in the comments with those lines

--- Irvine32Practice/main.asm
+++ Irvine32Practice/main.asm
@@ -24,14 +24,28 @@ Equation ENDP
 main PROC
 
 mov edx, OFFSET xValue		;Move the offset of xvalue to the edx register.
-call WriteString		;Display the string that is stored in the edx register.
-call ReadInt		;Read the next integer that the user inputs(This value should be saved to the eax register).
-mov xx, eax			;Store the value of eax into xx.
+call WriteString			;Display the string that is stored in the edx register.
+call ReadInt				;Read the next integer that the user inputs(This value should be saved to the eax register).
+mov xx, eax					;Store the value of eax into xx.
 
 mov edx, OFFSET yValue		;Move the offset of yvalue to the edx register.
-call WriteString		;Display the string that is stored in the edx register.
-call ReadInt		;Read the next integer that the user inputs.
-mov yy, eax		;Store the value of eax into yy.
+call WriteString			;Display the string that is stored in the edx register.
+call ReadInt				;Read the next integer that the user inputs.
+mov yy, eax					;Store the value of eax into yy.
+
+mov eax, xx					;Move the value of xx into the eax register.
+mov ebx, yy					;Move the value of yy into the ebx register.
+call Equation				;Call the Equation function.
+mov result, eax				;After the equation function has finished it will store the value in eax, this line will then store the value of eax to the result variable.
+
+mov edx, OFFSET resultText	;Move the offset value to the edx register
+call WriteString			;Display the string that is stored in the edx register
+mov eax, result				;Store the value of result in the eax register.
+call WriteInt				;Display the integer that is stored in the eax register
+call Crlf					;Moves the cursor to the next line
+
+call WaitMsg
+exit
 
 main ENDP
 END main
